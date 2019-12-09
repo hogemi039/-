1394cd1aadbc164c8c55995286d05ea0d2712fe1
hoge039 <umatama039@gmail.com>
2019-12-10 00:42:44 +0900

    no message

--- DXlibGame/DXlibGame/src/Map.hpp
+++ DXlibGame/DXlibGame/src/Map.hpp
@@ -11,23 +11,23 @@
 namespace
 {
 	////定数
-	const int MAP_WIDTH{ 10 };
-	const int MAP_HEIGHT{ 10 };
+	//const int MAP_WIDTH{ 10 };
+	//const int MAP_HEIGHT{ 10 };
 	const int GROUND_IMAGE_SIZE{ 32 };
-	const int map[MAP_HEIGHT][MAP_WIDTH] =
-	{
-		{0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,1,0,0,0,1},
+	//const int map[MAP_HEIGHT][MAP_WIDTH] =
+	//{
+	//	{0,0,0,0,0,0,0,0,0,0},
+	//	{0,0,0,0,0,0,0,0,0,0},
+	//	{0,0,0,0,0,0,0,0,0,0},
+	//	{1,0,0,0,0,0,0,0,0,1},
+	//	{1,0,0,0,0,1,0,0,0,1},
 
-		{1,0,0,0,1,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,1,1,1,1,1,1,1,1,1},
-		{0,0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0},
-	};
+	//	{1,0,0,0,1,0,0,0,0,1},
+	//	{1,0,0,0,0,0,0,0,0,1},
+	//	{1,1,1,1,1,1,1,1,1,1},
+	//	{0,0,0,0,0,0,0,0,0,0},
+	//	{0,0,0,0,0,0,0,0,0,0},
+	//};
 	MapManager *mapMng = new MapManager();
 	int maphandle[70];
 
@@ -35,35 +35,33 @@ namespace
 	{
 		DxLib::LoadDivGraph("resource/image/sheet.png", 70, 10, 7, 32, 32, maphandle);
 	}
-	void DrawMap()
-	{
-		for (int i = 0; i < MAP_HEIGHT; ++i)
-		{
-			for (int j = 0; j < MAP_WIDTH; ++j)
-			{
-				if (map[i][j] == 1)
-				{
-					//1番目のマップチップを描画させる
-					DxLib::DrawGraph(j * 32, i * 32, maphandle[1], true);
-				}
-			}
-		}
-	}
 	//void DrawMap()
 	//{
-	//	for (int y = 0; y < mapMng->GetMapHeight(); y++)
+	//	for (int i = 0; i < MAP_HEIGHT; ++i)
 	//	{
-	//		for (int x = 0; x < mapMng->GetMapWidth(); x++)
+	//		for (int j = 0; j < MAP_WIDTH; ++j)
 	//		{
-	//			if (mapMng->GetChipNum(x, y) != -1)
+	//			if (map[i][j] == 1)
 	//			{
-	//				DxLib::DrawGraph(x * 32, y * 32, maphandle[mapMng->GetChipNum(x, y)], true);
+	//				//1番目のマップチップを描画させる
+	//				DxLib::DrawGraph(j * 32, i * 32, maphandle[1], true);
 	//			}
 	//		}
 	//	}
 	//}
-
-
+	void DrawMap()
+	{
+		for (int y = 0; y < mapMng->GetMapHeight(); y++)
+		{
+			for (int x = 0; x < mapMng->GetMapWidth(); x++)
+			{
+				if (mapMng->GetChipNum(x, y) != -1)
+				{
+					DxLib::DrawGraph(x * 32, y * 32, maphandle[mapMng->GetChipNum(x, y)], true);
+				}
+			}
+		}
+	}
 
 	bool GetMapState(int x, int y)
 	{
